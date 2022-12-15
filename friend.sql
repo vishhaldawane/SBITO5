1a79716730414df2ce467604205db30b5577caef
vishhaldawane <45403067+vishhaldawane@users.noreply.github.com>
2022-12-15 14:56:23 +0530

    Add files via upload

---
+++ friend.sql
@@ -0,0 +1,12 @@
+create table friends
+(
+   friendId int primary key,
+   friendName varchar(20),
+   friendType varchar(20)
+);
+
+insert into friends values (1,'Deven','mutual');
+insert into friends values (2,'Samir','school');
+insert into friends values (3,'Priya','college');
+insert into friends values (4,'Jayant','office');
+insert into friends values (5,'YouAll','batch');
